cb176f0b3faf4068e90281d7a0a35b6322e3ab46
Darko Fabijan <darko@renderedtext.com>
2013-06-03 22:59:43 +0200

    Create and dummy view for website.

--- src/controller/sonar_website_controller.erl
+++ src/controller/sonar_website_controller.erl
@@ -4,3 +4,22 @@
 index('GET', []) ->
   Websites = boss_db:find(website, []),
   {ok, [{websites, Websites}]}.
+
+create('GET', []) ->
+  {ok, []};
+
+create('POST', []) ->
+  Url = xmerl_lib:export_text(Req:post_param("website_url")),
+  NewWebsite = website:new(id, Url),
+  case NewWebsite:save() of
+    {ok, SavedWebsite} ->  {redirect, [{action, "view"}, {id, SavedWebsite:id()}]};
+    {error, ErrorList} -> {ok, [{errors, ErrorList}, {new_website, NewWebsite}]}
+  end.
+
+testxxx('GET', [Id]) ->
+  case boss_db:find(Id) of
+    {error, Reason} -> {redirect, [{action, "index"}]};
+    undefined -> {redirect, [{action, "create"}]};
+    Website ->
+      {ok, [{website, Website}]}
+  end.
